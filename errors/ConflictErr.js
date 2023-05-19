2dd93f7a0452320afa899fb0b33a3c4fbbda40ff
Kristina Epifanova <cristinaepifanova@gmail.com>
2023-05-19 17:45:25 +0300

    add errors

---
+++ errors/ConflictErr.js
@@ -0,0 +1,10 @@
+const EMAIL_ERR = require('../utils/constans');
+
+class ConflictErr extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = EMAIL_ERR;
+  }
+}
+
+module.exports = ConflictErr;
